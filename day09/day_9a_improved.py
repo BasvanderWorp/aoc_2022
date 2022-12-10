bb8cae80d115d2cee2aec8ad2c40eb9e0737bbea
basvanderworp <bas.van.der.worp@navara.nl>
2022-12-10 22:54:40 +0100

    9b improved

--- day09/day_9a_improved.py
+++ day09/day_9a_improved.py
@@ -8,21 +8,22 @@ import numpy as np
 import sys
 
 test = False
-# test = True
+#test = True
 input_file = 'input.txt' if not test else 'input_test.txt'
 file_lines = read_input(input_file)
 
 sgn = lambda x: (x > 0) - (x < 0)
 h, t = (0, 0), (0, 0)
-D, Seen = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}, {t}
+dir = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
+seen = {t}
 
 for args in map(lambda x: x.strip().split(), file_lines):
     d, v = args[0], int(args[1])
     for _ in range(v):
-        h = tuple(sum(x) for x in zip(h, D[d]))
+        h = tuple(sum(x) for x in zip(h, dir[d]))
         dx, dy = h[0] - t[0], h[1] - t[1]
         if abs(dx) > 1 or abs(dy) > 1:
             t = tuple(sum(x) for x in zip(t, [sgn(dx), sgn(dy)]))
-        Seen.add(t)
+        seen.add(t)
 
-print(len(Seen))
+print(len(seen))
---
+++ day09/day_9b_improved.py
@@ -0,0 +1,31 @@
+"""
+Advent of Code 2022
+Day 9
+
+"""
+from helpers.io import read_input
+
+test = False
+# test = True
+input_file = 'input.txt' if not test else 'input_test2.txt'
+file_lines = read_input(input_file)
+
+sgn = lambda x: (x > 0) - (x < 0)
+dir = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
+rope = [(0,0) for _ in range(10)]
+seen = {rope[-1]}
+
+for args in map(lambda x: x.strip().split(), file_lines):
+    d, v = args[0], int(args[1])
+    for _ in range(v):
+        rope[0] = tuple(sum(x) for x in zip(rope[0], dir[d]))
+        for i in range(1, len(rope)):
+            dx, dy = rope[i - 1][0] - rope[i][0], rope[i - 1][1] - rope[i][1]
+            if abs(dx) > 1 or abs(dy) > 1:
+                rope[i] = tuple(sum(x) for x in zip(rope[i], (sgn(dx), sgn(dy))))
+        seen.add(rope[-1])
+
+print(len(seen))
+
+print('finished')
+
