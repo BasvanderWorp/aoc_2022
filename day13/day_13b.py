f2d1380062e984c76895e74397971ed4f52a6f00
basvanderworp <bas.van.der.worp@navara.nl>
2022-12-27 22:32:44 +0100

    day13b

--- day13/day_13b.py
+++ day13/day_13b.py
@@ -7,46 +7,61 @@ from helpers.io import read_input
 import numpy as np
 import copy
 
-def right_order(left_item, right_item):
+def convert_to_string(item):
+    if isinstance(item, list):
+        if len(item) > 0:
+            result = convert_to_string(item[0])
+        else:
+            result = '0'
+
+        if len(item) > 1:
+            result += convert_to_string(item[1:])
+        return result
+    else:
+        # item is int
+        result = str(item)
+        return result
+
+def smaller_than(left_item, right_item):
     if isinstance(left_item, list):
         if isinstance(right_item, list):
             if len(left_item) > 0:
                 if len(right_item) > 0:
-                    result = right_order(left_item[0], right_item[0])
+                    result = smaller_than(left_item[0], right_item[0])
                 else:
-                    result = 'false'
+                    result = False
             elif len(right_item) > 0:
-                result = 'true'
+                result = True
             else:
                 # both are empty, return equal
-                result = 'equal'
+                result = None
 
-            if result == 'equal':
+            if result == None:
                 if len(left_item) > 1:
                     if len(right_item) > 1:
-                        result = right_order(left_item[1:], right_item[1:])
+                        result = smaller_than(left_item[1:], right_item[1:])
                     else:
-                        result = 'false'
+                        result = False
                 elif len(right_item) > 1:
-                    result = 'true'
+                    result = True
                 else:
-                    result = 'equal'
+                    result = None
             return result
         else:
             # left is list, right is int
-            result = right_order(left_item, [right_item])
+            result = smaller_than(left_item, [right_item])
             return result
     else:
         if isinstance(right_item, list):
             # left is int, right is list
-            result = right_order([left_item], right_item)
+            result = smaller_than([left_item], right_item)
             return result
         else:
             # both ints
             if left_item == right_item:
-                result = 'equal'
+                result = None
             else:
-                result = 'true' if left_item < right_item else 'false'
+                result = left_item < right_item
             return result
 
 
@@ -58,14 +73,21 @@ test = False
 input_file = 'input.txt' if not test else 'input_test.txt'
 lines = read_input(input_file)
 
-pairs = list(zip([eval(line) for idx, line in enumerate(lines) if idx % 3 == 0],
-                 [eval(line) for idx, line in enumerate(lines) if (idx - 1) % 3 == 0]))
+items = [eval(line) for idx, line in enumerate(lines) if line != '']
 
+right_order_bool = np.array([], bool)
+right_order_bool_rev = np.array([], bool)
 right_order_indices = []
-for i, p in enumerate(pairs):
-    if right_order(p[0], p[1]) == 'true':
-        right_order_indices.append(i+1)
+sorted_items = [[[2]], [[6]]]
+for i, p in enumerate(items):
+    idx = 0
+    for item in sorted_items:
+        if smaller_than(p, item):
+            break
+        else:
+            idx += 1
+    sorted_items.insert(idx, p)
 
-print(f"Indices: {right_order_indices}")
+print(f"Decoder key: {(sorted_items.index([[2]]) + 1) * (sorted_items.index([[6]]) + 1)}")
 print()
-print(f"som: {sum(right_order_indices)}")
+
